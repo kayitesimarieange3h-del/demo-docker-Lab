ec882bec8aaf6fe2b8f0e3988daa700bee3acdd4
kayitesimarieange3h-del <kayitesimarieange3h@gmail.com>
2025-09-13 20:39:56 -0700

    dev infrastructure pipeline

--- remote_backend.tf
+++ remote_backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket       = "w7-kam-terr-buck"
     key          = "demo_docker/terraform.tfstate"
     region       = "us-east-1"
-    use_lockfile = false
+    # use_lockfile = false
   }
 }
