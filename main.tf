9d69d79632be384ca66da0164b49ff712b50d58a
kayitesimarieange3h-del <kayitesimarieange3h@gmail.com>
2025-09-13 20:56:58 -0700

    dev infrastructure pipeline

--- main.tf
+++ main.tf
@@ -148,7 +148,7 @@ data "aws_ami" "amazon_2" {
 
 resource "aws_instance" "DockerInstance" {
   ami                    = data.aws_ami.amazon_2.id
-  instance_type          = "t2.medium"
+  instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   key_name               = aws_key_pair.ec2_key.key_name
